1fbc846b4f4667abf283c5dd717c3c4661685acc
sphaso <sphaso@gmail.com>
2013-07-12 18:16:36 +0200

    ponnuki test added

--- simple_go_tests.py
+++ simple_go_tests.py
@@ -16,45 +16,47 @@ class TestSequenceFunctions(unittest.TestCase):
 
     def test_board_init(self):
         ''' tests __init__ in Board '''
-        self.assertEqual(self.board.size,self.size)
-        self.assertEqual(self.board.side,simple_go.BLACK)
-        self.assertEqual(self.board.captures[simple_go.BLACK],0)
-        self.assertEqual(self.board.captures[simple_go.WHITE],0)
-        self.assertEqual(self.board.groups[simple_go.BLACK],None)
-        self.assertEqual(self.board.groups[simple_go.WHITE],None)
-        self.assertEqual(self.board.territory_white,0)
-        self.assertEqual(self.board.territory_black,0)
+        self.assertEqual(self.board.size, self.size)
+        self.assertEqual(self.board.side, simple_go.BLACK)
+        self.assertEqual(self.board.captures[simple_go.BLACK], 0)
+        self.assertEqual(self.board.captures[simple_go.WHITE], 0)
+        self.assertEqual(self.board.groups[simple_go.BLACK], None)
+        self.assertEqual(self.board.groups[simple_go.WHITE], None)
+        self.assertEqual(self.board.territory_white, 0)
+        self.assertEqual(self.board.territory_black, 0)
         for pos in self.board.goban:
-            self.assertEqual(self.board.goban[pos],simple_go.EMPTY)
-        self.assertEqual(self.board.groups[simple_go.EMPTY],[self.board.goban.keys()])
+            self.assertEqual(self.board.goban[pos], simple_go.EMPTY)
+        self.assertEqual(self.board.groups[simple_go.EMPTY], [self.board.goban.keys()])
 
     def test_eq_ne(self):
         ''' tests __eq__ and __ne__ in Board'''
         board2 = deepcopy(self.board)
-        self.assertEqual(self.board,board2)
-        board2.make_move((4,4))
-        self.assertNotEqual(self.board,board2)
+        self.assertEqual(self.board, board2)
+        board2.make_move((4, 4))
+        self.assertNotEqual(self.board, board2)
 
     def test_iterate_goban(self):
         ''' test iterate_goban in Board '''
-        expected = self.size*self.size #expected value
+        expected = self.size * self.size  # expected value
         test = 0
         for pos in self.board.iterate_goban():
             test += 1
-        self.assertEqual(expected,test)
+        self.assertEqual(expected, test)
 
     def test_iterate_neighbour(self):
         ''' tests iterate_neighbour '''
         expected = 2
         test = 0
-        for pos in self.board.iterate_neighbour((1,1)):
+        for pos in self.board.iterate_neighbour((1, 1)):
             test += 1
-        self.assertEqual(expected,test)
+        self.assertEqual(expected, test)
 
     def test_firstMove(self):
         pos = (3, 3)
         self.game.make_move(pos)
         self.assertEqual(self.game.current_board.goban[pos], simple_go.BLACK)
+        # TOTHINK: side doesn't change until the next move? this fails'
+        self.assertEqual(self.board.side, simple_go.WHITE)
 
     def test_is_neighbour(self):
         pos = (3, 3)
@@ -81,12 +83,31 @@ class TestSequenceFunctions(unittest.TestCase):
         self.game.make_move(pos)
         self.assertEqual(self.board.liberties(pos), 4)
 
+    def test_ponnuki(self):
+        pos = (2, 3)
+        pos2 = (3, 2)
+        pos3 = (4, 3)
+        pos4 = (3, 4)
+        self.board.side = simple_go.BLACK
+        self.board.make_move(pos, False)
+        self.board.make_move(pos2, False)
+        self.board.make_move(pos3, False)
+        self.board.make_move(pos4, False)
+        self.assertEqual(self.board.groups[simple_go.BLACK], [[(2, 3)], [(3, 2)], [(4, 3)], [(3, 4)]])
+        self.board.side = simple_go.WHITE
+        # TOTHINK: this should not fail, suicide
+        self.assertEqual(self.game.legal_move((3, 3)), False)
+        self.assertEqual(self.board.liberties(pos), 4)
+        self.assertEqual(self.board.liberties(pos2), 4)
+        self.assertEqual(self.board.liberties(pos3), 4)
+        self.assertEqual(self.board.liberties(pos4), 4)
+
     def test_string_move_functions(self):
         ''' test string/move conversion functions '''
-        test = {(1,1):"A1", (2,3):"B3", (5,5):"E5"}
-        for pos,string in test.items():
-            self.assertEqual(simple_go.move_as_string(pos,self.size),string)
-            self.assertEqual(simple_go.string_as_move(string,self.size),pos)
+        test = {(1, 1): "A1", (2, 3): "B3", (5, 5): "E5"}
+        for pos, string in test.items():
+            self.assertEqual(simple_go.move_as_string(pos, self.size), string)
+            self.assertEqual(simple_go.string_as_move(string, self.size), pos)
 
 if __name__ == '__main__':
     unittest.main()
